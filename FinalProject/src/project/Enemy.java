39100929af10130f4e14d0c2ae4d117dd973a026
kiranawarrier <warriek@rose-hulman.edu>
2025-10-28 16:43:18 -0400

    Added to enemy class

--- FinalProject/src/project/Enemy.java
+++ FinalProject/src/project/Enemy.java
@@ -3,6 +3,9 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 public class Enemy{
 	private int x,y;
+	private static final int WIDTH = 70;
+	private static final int HEIGHT = 70;
+	
 	
 	Color color = Color.RED;
 	private int dx = 4;
@@ -14,8 +17,8 @@ public class Enemy{
 	public void move() {
 		x += dx;
 	}
-	public void draw(Graphics2D g2) {
+	public void drawEnemy(Graphics2D g2) {
 	    g2.setColor(color);
-	    g2.fillRect(x, y, x,y);
+	    g2.fillRect(x, y, WIDTH, HEIGHT);
 	}
 }
