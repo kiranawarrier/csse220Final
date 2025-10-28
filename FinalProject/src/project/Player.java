50418ec452b41a83fb952373b5503b71d5d1cd85
azumahm <azumahm@rose-hulman.edu>
2025-10-28 16:24:44 -0400

    trying to create the enenmy and player

--- FinalProject/src/project/Player.java
+++ FinalProject/src/project/Player.java
@@ -1,5 +1,18 @@
 package project;
 
-public class Player extends Entity {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class Player {
+	int x,y;
+	Color color = Color.GREEN;
 	
+	protected void paintPlayer(Graphics g) {
+	//super.paintPlayer(g);
+	Graphics2D g2 = (Graphics2D) g;
+	g.setColor(color);
+	g.drawRect(x, y, y, x);
+	
+}
 }
