2c98a8e60222ba94de3f836334edb11560f63473
ajothi <ajothi@Latitude>
2025-10-28 16:48:48 -0400

    Scoreboard

--- FinalProject/src/project/Scoreboard.java
+++ FinalProject/src/project/Scoreboard.java
@@ -5,6 +5,23 @@ public class Scoreboard {
 	private int score = 0;
 	private int livesLeft = 3;
 	
+	public Scoreboard(int livesLeft) {
+		this.livesLeft = livesLeft;
+	}
+	
+	public Scoreboard() {
+		
+	}
+	
+	public void updateScore() {
+		
+	}
+	
+	public void display() {
+		
+	}
+	
+	
 	
 
 }
