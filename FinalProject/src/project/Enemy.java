e12230dbf15cb87e23562554f19262ba016484bc
kiranawarrier <warriek@rose-hulman.edu>
2025-10-28 16:39:49 -0400

    Update Enemy.java

--- FinalProject/src/project/Enemy.java
+++ FinalProject/src/project/Enemy.java
@@ -2,20 +2,20 @@ package project;
 import java.awt.Color;
 import java.awt.Graphics2D;
 public class Enemy{
- int x,y;
- Color color = Color.RED;
- private int dx = 1;
- private int dy = 2;
- public Enemy(int x, int y) {
-	 this.x = x; this.y = y;
- }
- 
- public void Move(int width, int height) {
-	 // add random movements using dy and dx 
+	private int x,y;
+	
+	Color color = Color.RED;
+	private int dx = 4;
 	 
- }
- public void draw(Graphics2D g2) {
-     g2.setColor(color);
-     g2.fillRect(x, y, x,y);
- }
+	public Enemy(int x, int y) {
+		this.x = x; this.y = y;
+	}
+	 
+	public void move() {
+		x += dx;
+	}
+	public void draw(Graphics2D g2) {
+	    g2.setColor(color);
+	    g2.fillRect(x, y, x,y);
+	}
 }
