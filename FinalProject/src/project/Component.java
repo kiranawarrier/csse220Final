f9a99cea319ab53795b40ffc56cf61ab92c83b94
azumahm <azumahm@rose-hulman.edu>
2025-10-30 15:02:54 -0400

    change the enenmy to gorund level

--- FinalProject/src/project/Component.java
+++ FinalProject/src/project/Component.java
@@ -13,7 +13,7 @@ public class Component extends JComponent{
 	public static final Color BG = new Color(18, 29, 57);
 	public static final Color FG = new Color(8, 128, 38);
 	Player player = new Player(200,630);
-	Enemy enemy = new Enemy(1000,700);
+	Enemy enemy = new Enemy(1000,630);
 	
 	public Component() {
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
