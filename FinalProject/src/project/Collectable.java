e6741fe22fd86bb1f5aabe5e84d2edf091eda5f7
Arjun Jothi <jothira@rose-hulman.edu>
2025-11-03 14:04:27 -0500

    Updated the Collectable.java by adding getters for collision identification

--- FinalProject/src/project/Collectable.java
+++ FinalProject/src/project/Collectable.java
@@ -18,9 +18,30 @@ public class Collectable {
 
     public void drawCollectable(Graphics2D g2) {
         g2.setColor(color);
-        g2.fillRect(y, x, WIDTH, HEIGHT);
+        g2.fillRect(x, y, WIDTH, HEIGHT);
     }
-    public void pickup() {
 
+    public void pickup() {
+        this.isVisible = false;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isVisible = true;
+
+    public int getWidth() {
+        return WIDTH;
+    }
+
+    public int getHeight() {
+        return HEIGHT;
+    }
+
+
 }
