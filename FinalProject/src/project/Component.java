ab3b583888dae7897fd77b537f8deb5be40f5600
kiranawarrier <warriek@rose-hulman.edu>
2025-11-03 16:23:06 -0500

    made eney hitbox affect all of player model

--- FinalProject/src/project/Component.java
+++ FinalProject/src/project/Component.java
@@ -135,7 +135,7 @@ public class Component extends JComponent{
         
     }
     private void enemyCollisions() {
-    	if ((player.getX() >= enemy.getX() && player.getX() <= enemy.getX() + 70) && player.getY() >= enemy.getY()) {
+    	if ((player.getX() >= enemy.getX() && player.getX() <= enemy.getX() + 70) && player.getY() >= enemy.getY() - 110) {
     		player.die();
     		score.dead();
     		System.out.println("You Died");
