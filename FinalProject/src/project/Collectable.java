9070a9a0bdb5286279893d752b789d7e8e8a69c2
Arjun Jothi <jothira@rose-hulman.edu>
2025-11-03 15:34:23 -0500

    added coin.png

--- FinalProject/src/project/Collectable.java
+++ FinalProject/src/project/Collectable.java
@@ -1,26 +1,51 @@
 package project;
 
+import javax.imageio.ImageIO;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Collectable {
 
     private int x, y;
     private static final int WIDTH = 20;
     private static final int HEIGHT = 20;
-    public boolean isVisible = true;
+    public boolean isVisible = false;
 
+    private BufferedImage sprite;
+    private boolean spriteLoaded = false;
+
+    public static final int RENDERED_WIDTH = 40;
+    public static final int RENDERED_HEIGHT = 40;
 
     Color color = Color.YELLOW;
 
     public Collectable(int x, int y) {
         this.x = x;
         this.y = y;
+
+        try {
+            sprite = ImageIO.read(Objects.requireNonNull(Player.class.getResource("coin.png")));
+            spriteLoaded = (sprite != null);
+        } catch (IOException | IllegalArgumentException ex) {
+            spriteLoaded = false;
+            System.out.println("FAILED: " + Player.class.getResource("coin.png"));
+            System.out.print("  coin failed to load");
+        }
     }
 
     public void drawCollectable(Graphics2D g2) {
-        g2.setColor(color);
-        g2.fillRect(x, y, WIDTH, HEIGHT);
+        int drawX = x;
+        int drawY = y;
+
+        if (spriteLoaded) {
+            g2.drawImage(sprite, drawX, drawY, RENDERED_WIDTH, RENDERED_HEIGHT, null);
+        } else {
+            g2.setColor(color);
+            g2.fillRect(x, y, WIDTH, HEIGHT);
+        }
     }
 
     public void pickup() {
@@ -36,14 +61,15 @@ public class Collectable {
     }
 
     public int getWidth() {
-        return WIDTH;
+        return spriteLoaded ? RENDERED_WIDTH : WIDTH;
     }
 
     public int getHeight() {
-        return HEIGHT;
+        return spriteLoaded ? RENDERED_HEIGHT : HEIGHT;
     }
 
     public boolean isVisible() {
         return isVisible;
     }
+
 }
