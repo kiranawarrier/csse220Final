7843b0e36434cfc2948ad3077593499deab0c194
kiranawarrier <warriek@rose-hulman.edu>
2025-11-04 16:26:26 -0500

    added a screen class and created a timer for end game

---
+++ FinalProject/src/project/Screen.java
@@ -0,0 +1,44 @@
+package project;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Screen {
+	public static final int WIDTH = 1920;
+	public static final int HEIGHT = 1080;
+	
+	public static final Color BG = new Color(18, 29, 57);
+	public static final Color FG = new Color(8, 128, 38);
+	
+	
+	ImageIcon endScreen = new ImageIcon("endScreen.png");
+    JLabel endScreenLabel = new JLabel(endScreen);
+    Font topFont = new Font(Font.DIALOG_INPUT, Font.BOLD, 100);
+
+    Font bottomFont = new Font(Font.DIALOG_INPUT, Font.BOLD, 30);
+    
+    public Screen() {
+    	
+    }
+    
+    public void displayScreen(Graphics2D g2){
+    	
+    }
+    
+    public void displayEndScreen(Graphics2D g2, int timeLeft) {
+    	g2.setColor(Color.black);
+    	g2.fillRect(0, 0, WIDTH, HEIGHT);
+    	
+    	g2.setColor(Color.red);
+    	g2.setFont(topFont);
+    	g2.drawString("Game Over",500,400);
+    	
+    	g2.setColor(Color.white);
+    	g2.setFont(bottomFont);
+    	g2.drawString("Restarting in: " + timeLeft + " seconds", 553, 450);
+    }
+}
