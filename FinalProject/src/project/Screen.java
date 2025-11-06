90ef0e0950de051edb994879dc3e6fc7123bbfcf
kiranawarrier <warriek@rose-hulman.edu>
2025-11-06 16:38:10 -0500

    added background and platform images

--- FinalProject/src/project/Screen.java
+++ FinalProject/src/project/Screen.java
@@ -3,7 +3,11 @@ package project;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
@@ -12,7 +16,11 @@ public class Screen {
 	public static final int HEIGHT = 1080;
 	
 	public static final Color BG = new Color(18, 29, 57);
+	public static final Color MG = new Color (0, 75, 68);
 	public static final Color FG = new Color(8, 128, 38);
+	
+	private BufferedImage background;
+	private boolean bgLoaded;
 	
 	
 	ImageIcon endScreen = new ImageIcon("endScreen.png");
@@ -22,15 +30,37 @@ public class Screen {
     Font bottomFont = new Font(Font.DIALOG_INPUT, Font.BOLD, 30);
     
     public Screen() {
-    	
+    	try {
+    		background = ImageIO.read(Objects.requireNonNull(Player.class.getResource("FinalProjectBackground.png")));
+    		bgLoaded = (background != null);
+    	}catch(IOException | IllegalArgumentException ex){
+    		bgLoaded = false;
+    		System.out.println("FAILED: " + Player.class.getResource("FinalProjectBackground.png"));
+            System.out.print("  background failed to load");
+    	}
     }
     
     public void displayScreen(Graphics2D g2){
-    	g2.setColor(BG);
-    	g2.fillRect(0, 0, WIDTH, HEIGHT);
-    	
-    	g2.setColor(FG);
-    	g2.fillRect(0, 700, WIDTH, HEIGHT - 700);
+    	if(bgLoaded) {
+    		g2.drawImage(background, 0, 0, WIDTH, HEIGHT, null);
+    	}
+    	else {
+			g2.setColor(BG);
+	    	g2.fillRect(0, 0, WIDTH, HEIGHT);
+	    	
+	    	g2.setColor(MG); //draw all the middle ground objects
+	    	g2.fillRoundRect(300, 400, 250, 700, 250, 250);
+	    	g2.fillRoundRect(100, 600, 50, 700, 50, 50);
+	    	g2.fillRoundRect(200, 500, 50, 700, 50, 50);
+	    	
+	    	g2.fillRoundRect(900, 200, 250, 700, 250, 250);
+	    	g2.fillRoundRect(800, 300, 50, 700, 50, 50);
+	    	g2.fillRoundRect(700, 450, 50, 700, 50, 50);
+	    	g2.fillRoundRect(600, 400, 50, 700, 50, 50);
+	    	
+	    	g2.setColor(FG);
+	    	g2.fillRect(0, 700, WIDTH, HEIGHT - 700);
+    	}
     }
     
     public void displayEndScreen(Graphics2D g2, int timeLeft) {
