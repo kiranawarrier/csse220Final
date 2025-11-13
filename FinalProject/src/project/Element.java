084dc66e01248d5ad59b770e1adcf27486b820ba
Arjun Jothi <jothira@rose-hulman.edu>
2025-11-13 15:28:10 -0500

    Element.java updated

--- FinalProject/src/project/Element.java
+++ FinalProject/src/project/Element.java
@@ -1,19 +1,24 @@
 package project;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public abstract class Element {
 
     private int x, y;
     private int width, height;
     private Color color;
+    private BufferedImage sprite;
+    private boolean spriteLoaded;
 
-    public Element(int x, int y, int width, int height, Color color) {
+    public Element(int x, int y, int width, int height, Color color,  BufferedImage sprite,  boolean spriteLoaded) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.color = color;
+        this.sprite = sprite;
+        this.spriteLoaded = spriteLoaded;
     }
 
     public int getX() {
